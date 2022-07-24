a4bd53adb6c82fb7bd10db4d09d5a23fbddc7bbc
Nithin BS <83852776+nithin2027@users.noreply.github.com>
2022-07-24 15:02:33 +0530

    Update Merge_sort.c

--- Merge_sort.c
+++ Merge_sort.c
@@ -1,117 +1,63 @@
-#include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
-#include <math.h>
-void split(int[],int,int);
-void combine(int[],int,int,int);
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+void mergesort(int a[],int i,int j);
+
+void merge(int a[],int i1,int j1,int i2,int j2);
+
 int main()
 {
-    int a[15000], n, i, j, ch, temp;
-    clock_t start, end;
-    while (1)
-    {
-        printf("\n1:FOR MANUAL ENTRY");
-        printf("\n2:DISPLAY TIME TAKEN TO SORT ELEMENTS FROM RANGE 500 TO 15000");
-        printf("\n3:EXIT");
-        printf("\nENTER YOUR CHOICE:");
-        scanf("%d", &ch);
-        switch (ch)
-        {
-        case 1:
-            printf("\nENTER NUMBER OF ARRAY ELEMENTS: ");
-            scanf("%d", &n);
-            printf("\nENTER ARRAY ELEMENTS: ");
-            for (i = 0; i < n; i++)
-            {
-                scanf("%d", &a[i]);
-            }
-            start = clock();
-            split(a, 0, n - 1);
-            end = clock();
-            printf("\nSORTED ARRAY IS: ");
-            for (i = 0; i < n; i++)
-                printf("%d\t", a[i]);
-            printf("\n TIME TAKEN TO SORT %d NUMBERS IS %f SECS", n, (((double)(end - start)) / CLOCKS_PER_SEC));
-            break;
-        case 2:
-            n = 500;
-            while (n <= 14500)
-            {
-                for (i = 0; i < n; i++)
-                {
-                     a[i]=rand()%n;
-                    //a[i] = n - i;
-                }
-                start = clock();
-                split(a, 0, n - 1);
-                for (j = 0; j < 500000; j++)
-                {
-                    temp = 38 / 600;
-                }
-                end = clock();
-                printf("\n TIME TAKEN TO SORT %d NUMBERS IS %f SECS", n, (((double)(end - start)) / CLOCKS_PER_SEC));
-                n = n + 1000;
-            }
-            break;
-        case 3:
-            exit(0);
-        }
-        getchar();
-    }
+clock_t start,end;
+int a[30000],n=500,i;
+while(n<=5000){
+for(i=0;i<n;i++)
+{
+a[i] = rand()%1000;
+}
+start = clock();
+mergesort(a,0,n-1);
+end = clock();
+printf("\nSorted array of %d numbers =  ",n);
+printf("Seconds taken %lf",(double)(end-start)/CLOCKS_PER_SEC);
+printf(“\n”);
+n+=500;
+}
 }
 
-void split(int a[], int low, int high)
+void mergesort(int a[],int i,int j)
 {
-    int mid;
-    if (low < high)
-    {
-        mid = (low + high) / 2;
-        split(a, low, mid);
-        split(a, mid + 1, high);
-        combine(a, low, mid, high);
-    }
+int mid;
+if(i<j)
+{
+mid=(i+j)/2;
+mergesort(a,i,mid);
+mergesort(a,mid+1,j);
+merge(a,i,mid,mid+1,j);
+}
 }
 
-void combine(int a[], int low, int mid, int high)
+void merge(int a[],int i1,int j1,int i2,int j2)
 {
-    int c[15000], i, j, k;
-    i = k = low;
-    j = mid + 1;
-    while (i <= mid && j <= high)
-    {
-        if (a[i] < a[j])
-        {
-            c[k] = a[i];
-            ++k;
-            ++i;
-        }
-        else
-        {
-            c[k] = a[j];
-            ++k;
-            ++j;
-        }
-    }
-    if (i > mid)
-    {
-        while (j <= high)
-        {
-            c[k] = a[j];
-            ++k;
-            ++j;
-        }
-    }
-    if (j > high)
-    {
-        while (i <= mid)
-        {
-            c[k] = a[i];
-            ++k;
-            ++i;
-        }
-    }
-    for (i = low; i <= high; i++)
-    {
-        a[i] = c[i];
-    }
+int temp[30000];
+int i,j,k;
+i=i1;
+j=i2;
+k=0;
+while(i<=j1 && j<=j2)
+{
+    for(int j=0;j<100000;j++);
+if(a[i]<a[j])
+temp[k++]=a[i++];
+else
+temp[k++]=a[j++];
+}
+while(i<=j1)
+temp[k++]=a[i++];
+while(j<=j2)
+temp[k++]=a[j++];
+
+for(i=i1,j=0;i<=j2;i++,j++){
+a[i]=temp[j];
+}
 }
