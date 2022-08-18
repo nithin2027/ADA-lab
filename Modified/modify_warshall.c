ac4fa29c863d2a2ebcdf34294dca628d8feb4ebf
Nithin BS <83852776+nithin2027@users.noreply.github.com>
2022-08-18 21:30:18 +0530

    Create modify_warshall.c

---
+++ Modified/modify_warshall.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include<stdlib.h>
+int a[30][30];
+
+void warshall(int n)
+{
+    for (int k = 1; k <= n; k++)
+    {
+        for (int i = 1; i <= n; i++)
+            for (int j = 1; j <= n; j++)
+                a[i][j] = a[i][j] || (a[i][k] && a[k][j]);
+    }
+}
+
+int main()
+{
+    int n;
+    printf("Enter no of vertices: \n");
+    scanf("%d", &n);
+
+    printf("Enter adjacency matrix: \n");
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++)
+            scanf("%d", &a[i][j]);
+    warshall(n);
+    printf("Transitive Closure: \n");
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+            printf("%d ", a[i][j]);
+        printf("\n");
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        /* code */
+        if (a[i][i] == 1)
+        {
+            printf("CYCLE EXISTS");
+            exit(0);
+        }
+    }
+}
